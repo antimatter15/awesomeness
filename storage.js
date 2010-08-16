5c17eb0545ad9762ad0765e76e3d642e7682f410
antimatter15 <antimatter15@gmail.com>
2010-08-16 02:11:24 +0200

    stuff

--- storage.js
+++ storage.js
@@ -1,8 +1,23 @@
 //Storage
 
-var msgs = {}
-var globalacl = {
+var msgs = {
+	'test': {
+		acl: {
+			def: {}
+		},
+		id: 'test',
+		v: 0,
+		children: [],
+		text: 'hello world'
+	}
 	
+}
+var globalacl = {
+	write: true, //without this anything else wouldnt work
+	write_acl: true,
+	add_children: true,
+	read: true,
+	change_text: true
 };
 
 //crappy diff algorithm which handles simple replace cases
@@ -31,25 +46,53 @@ function getACL(host, msg){
 
 function applyDelta(id, host, delta){
 	var msg = msgs[id];
+	
+	if(delta.v != msg.v){
+		//version mismatch. FAIL
+		return false;
+	}
+	
 	var can = getACL(host, msg);
+	if(can.write){
+		msg.time = +new Date;
+		msg.v++; //increment version
 	
-	msg.time = +new Date;
-	msg.v++; //increment version
-	
-	if(can.write_acl){
-		for(var i in delta.acl){
-			msg.acl[i] = msg.acl[i] || {};
-			for(var k in delta.acl[i])
-				msg.acl[i][k] = delta.acl[i][k];
+		if(can.write_acl && delta.acl){
+			for(var i in delta.acl){
+				msg.acl[i] = msg.acl[i] || {};
+				for(var k in delta.acl[i])
+					msg.acl[i][k] = delta.acl[i][k];
+			}
+		}
+		
+		if(can.add_children && delta.add_children){
+			//TODO: support Reordering
+			msg.children = msg.children.concat(delta.add_children);
+		}
+		
+		//A *very* basic totally not working real OT that will have
+		//TONS OF COLLISIONS. DO NOT USE THIS IN ANYTHING OTHER THAN
+		//A PROTOTYPE!
+		if(can.change_text && delta.ot){
+			for(var i = 0, l = delta.ot.length; i < l; i++){
+				var r = delta.ot[i]; //[14, 18, "gray"]
+				msg.text = msg.text.substr(0,r[0]) + r[2] + msg.text.substr(r[1]);
+			}
 		}
 	}
-	
-	//A *very* basic totally not working real OT that will have
-	//TONS OF COLLISIONS. DO NOT USE THIS IN ANYTHING OTHER THAN
-	//A PROTOTYPE!
-	for(var i = 0, l = delta.ot.length; i < l; i++){
-		var r = delta.ot[i]; //[14, 18, "gray"]
-		msg.text = msg.text.substr(0,r[0]) + r[2] + msg.text.substr(r[1]);
-	}
-	
 }
+
+
+
+
+
+console.log(msgs.test.text)
+
+applyDelta('test', 'nothing', {
+	ot: [
+		[5,6,' shiny ']
+	],
+	add_children: ['googlepoop']
+})
+
+console.log(msgs.test)
